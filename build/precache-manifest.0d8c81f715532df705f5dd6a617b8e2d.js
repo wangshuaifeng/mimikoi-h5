abe9f5ead04d05d362cedff284f222f6c05e5b5c
shuaifeng.wang <sfwang.wang@weimob.com>
2019-10-27 22:50:05 +0800

    .

---
+++ build/precache-manifest.0d8c81f715532df705f5dd6a617b8e2d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91e12eae60ebaa7a1e3dd4079d407e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66f3d99ea896c6e18e5f",
+    "url": "/static/css/2.76f0389b.chunk.css"
+  },
+  {
+    "revision": "a116d8975759d068da27",
+    "url": "/static/css/main.d704ffe6.chunk.css"
+  },
+  {
+    "revision": "66f3d99ea896c6e18e5f",
+    "url": "/static/js/2.a7d36b34.chunk.js"
+  },
+  {
+    "revision": "a116d8975759d068da27",
+    "url": "/static/js/main.8fc598e3.chunk.js"
+  },
+  {
+    "revision": "ffc3a64134badb7be9de",
+    "url": "/static/js/runtime-main.eae97b2b.js"
+  },
+  {
+    "revision": "a116340a28a7554f6d7adfa1a6ece7cc",
+    "url": "/static/media/1.a116340a.png"
+  },
+  {
+    "revision": "0494a9e86acb3cc793e7fff84f6af357",
+    "url": "/static/media/10.0494a9e8.png"
+  },
+  {
+    "revision": "2fea961b9cd2fbb5e00001e1a4f8a320",
+    "url": "/static/media/11.2fea961b.png"
+  },
+  {
+    "revision": "e1d6f9109b2a6e5fe24090a061280a5c",
+    "url": "/static/media/3.e1d6f910.png"
+  },
+  {
+    "revision": "2552c8ce139f5de5d12f7ec23d4c3abf",
+    "url": "/static/media/6.2552c8ce.png"
+  },
+  {
+    "revision": "75fc66712ac84d6498f4a6bed3674190",
+    "url": "/static/media/8.75fc6671.png"
+  },
+  {
+    "revision": "7dba62f1cac6cd89b8f475694735a4c4",
+    "url": "/static/media/9.7dba62f1.png"
+  },
+  {
+    "revision": "00b5162761e130dd258377b012836bb0",
+    "url": "/static/media/american.00b51627.svg"
+  },
+  {
+    "revision": "1bcb6f660c87ea5c80d4e1dfc7364fdd",
+    "url": "/static/media/bg.1bcb6f66.png"
+  },
+  {
+    "revision": "2b5f1076e59bb0e71ac530fa4ef7db15",
+    "url": "/static/media/diners.2b5f1076.svg"
+  },
+  {
+    "revision": "9eee0a0dabfb8d94e448cc99470d3d9b",
+    "url": "/static/media/jcb.9eee0a0d.svg"
+  },
+  {
+    "revision": "2a93f7881a200bf4b24d3b77aab87a63",
+    "url": "/static/media/masterCard.2a93f788.svg"
+  },
+  {
+    "revision": "8afe2c412e0a049f6a68e2cbb51f05f1",
+    "url": "/static/media/particular.8afe2c41.jpg"
+  },
+  {
+    "revision": "05d2d57a68e6928c17a881955c205652",
+    "url": "/static/media/visa.05d2d57a.svg"
+  }
+]);
